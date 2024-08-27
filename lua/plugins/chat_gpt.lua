05c93cf30e5da4c10bb84991cb12662fbd971be0
Denis <denis.shvetsov@quantpi.com>
2024-08-27 22:19:34 +0200

    update chatgpt plugin

--- lua/plugins/chat_gpt.lua
+++ lua/plugins/chat_gpt.lua
@@ -2,33 +2,31 @@ return {
   {
     "jackMort/ChatGPT.nvim",
       event = "VeryLazy",
-      config = function()
-        require("chatgpt").setup()
-      end,
       dependencies = {
         "MunifTanjim/nui.nvim",
         "nvim-lua/plenary.nvim",
         "folke/trouble.nvim",
         "nvim-telescope/telescope.nvim"
-      }
-  },
-  config = function()
-    local chatgpt = require("chatgpt")
-    local wk = require("which-key")
-    wk.register({
-        gp = {
-            name = "ChatGPT",
-            t = {
-                function()
-                    chatgpt.edit_with_instructions()
-                end,
-                "Edit with instructions",
-            },
-        },
-    }, {
-        prefix = "<leader>",
-        mode = "v",
-    })
+      },
+    config = function()
+      local chatgpt = require("chatgpt")
+      chatgpt.setup()
+      local wk = require("which-key")
+      wk.register({
+          gp = {
+              name = "ChatGPT",
+              t = {
+                  function()
+                      chatgpt.edit_with_instructions()
+                  end,
+                  "Edit with instructions",
+              },
+          },
+      }, {
+          prefix = "<leader>",
+          mode = "v",
+      })
 
-  end
+    end
+  },
 }

8ce29c7395a648b6ac327321feb503d520033e37
Denis <denis.shvetsov@quantpi.com>
2024-08-27 22:11:28 +0200

    put python to exceptions

--- lua/plugins/standard.lua
+++ lua/plugins/standard.lua
@@ -11,4 +11,23 @@ return {
   'scrooloose/nerdtree',
   'scrooloose/nerdcommenter',
   'windwp/nvim-autopairs',
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {},
+  },
+  { 'numToStr/Comment.nvim', opts = {} },
 }

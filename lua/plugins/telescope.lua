797ee27f6c55732430a57ca71fdf9fdd490bd846
Denis <denis.shvetsov@quantpi.com>
2024-08-29 12:28:07 +0200

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -87,7 +87,7 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-      local success, diagnostic_f = pcall(require('diagnostic_selector').get)
+      local success, diagnostic_f = pcall(function() return require('diagnostic_selector').get end)
       if (not success)
       then
         diagnostic_f = builtin.diagnostics
